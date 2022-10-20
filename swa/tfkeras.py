cf46fd80569170322f6877e54719f2b9a5990cf2
Anabetsy Rivero <arivero@eeposit.com>
2022-10-20 15:50:08 -0400

    first commit

--- swa/tfkeras.py
+++ swa/tfkeras.py
@@ -1,9 +1,11 @@
 """ Tensorflow Keras SWA Object
 """
 
-import tensorflow.keras.backend as K
+#import tensorflow.keras.backend as K
+from torchbearer import Trial
+
 from tensorflow.keras.callbacks import Callback
 from tensorflow.keras.layers import BatchNormalization
 from .callback import create_swa_callback_class
 
-SWA = create_swa_callback_class(K, Callback, BatchNormalization)
+SWA = create_swa_callback_class(Trial, Callback, BatchNormalization)
